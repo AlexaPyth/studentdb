5e5aae74bea6c0e12ed69e08f2c10eee805e4519
Alex Pro Django <subintuit@gmail.com>
2015-10-18 21:34:20 +0300

    косметична помилка

--- students/views.py
+++ students/views.py
@@ -19,7 +19,7 @@ def students_list(request):
          'first_name': 'Віталій',
          'last_name': 'Пдоба',
          'ticket': 2127,
-         'image': 'iimg/podoba.jpg'},
+         'image': 'img/podoba.jpg'},
     )
     return render(request, "students/students_list.html", {'students':students})
 
