ed120f327a6f72c3986f99a623aefa92ca025692
Alex Pro Django <subintuit@gmail.com>
2015-10-19 19:01:18 +0300

    # Моя змінна - абсолютне посилання на корінь аплікації. PORTAL_URL = 'http://localhost:8000'

--- studentdb/settings.py
+++ studentdb/settings.py
@@ -15,6 +15,8 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Моя змінна - абсолютне посилання на корінь аплікації.
+PORTAL_URL = 'http://localhost:8000'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
