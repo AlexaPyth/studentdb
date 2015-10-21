51ceb59dd3a84885579ca80d4a29923b66e0f8d2
Alex Pro Django <subintuit@gmail.com>
2015-10-21 20:04:54 +0300

    динамічно обчислює кореневу абсолютну адресу веб-сайту

--- studentdb/context_processors.py
+++ studentdb/context_processors.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from .settings import PORTAL_URL
+# from .settings import PORTAL_URL
 
 def students_proc(request):
-    return {'PORTAL_URL': PORTAL_URL}
+
+    return {'PORTAL_URL': request.build_absolute_uri('/')[:-1]}
