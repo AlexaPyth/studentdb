3ac0831d6e77a0ae97ec8f0d784d3060ae051a16
AlexaPyth <shakam77@gmail.com>
2015-10-22 14:37:35 +0300

    добавив для тесту список студентів для виводу журналу, вказав їх в рендері

--- students/views/journal.py
+++ students/views/journal.py
@@ -5,5 +5,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 #Views for Journal
+
+students = (
+        {'id':1,
+         'first_name': 'Андрій',
+         'last_name': 'Корост'},
+        {'id':2,
+         'first_name': 'Орест',
+         'last_name': 'Пивовар'},
+        {'id': 3,
+         'first_name': 'Віталій',
+         'last_name': 'Пдоба'},
+    )
+
+
+
 def journal(request):
-    return render(request, "students/journal.html", {})
+    return render(request, "students/journal.html", {'students_j':students})
