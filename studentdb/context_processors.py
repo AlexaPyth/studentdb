c1799a1dcee2728a902ae8dc99834eee892bc1f6
AlexaPyth <shakam77@gmail.com>
2015-10-22 12:35:02 +0300

    підглянув у Віталі - перевірку

--- studentdb/context_processors.py
+++ studentdb/context_processors.py
@@ -2,5 +2,7 @@
 # from .settings import PORTAL_URL
 
 def students_proc(request):
-
-    return {'PORTAL_URL': request.build_absolute_uri('/')[:-1]}
+    portal_url = request.build_absolute_uri('/')
+    if portal_url.endswith('/'):
+        portal_url = portal_url[:-1]
+    return {'PORTAL_URL': portal_url}
