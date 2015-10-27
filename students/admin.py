b6632592e9b7830d5483ff4bcb64e91822984a58
AlexaPyth <shakam77@gmail.com>
2015-10-27 16:05:58 +0200

    добавив полу Група - в модель Студента +  модель Групи + реєстрація Group-model в admin.py

--- students/admin.py
+++ students/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from students.models import Student
-
+from students.models import Group
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(Group)
--- students/models.py
+++ students/models.py
@@ -41,6 +41,12 @@ class Student(models.Model):
         blank=False,
         verbose_name="Білет")
 
+    student_group = models.ForeignKey('Group',
+        verbose_name='Група',
+        blank=False,
+        null=True,
+        on_delete=models.PROTECT)
+
     notes = models.TextField(
         blank=True,
         verbose_name="Додаткові нотатки",
@@ -50,3 +56,32 @@ class Student(models.Model):
     def __str__(self):
         return "%s %s" % (self.first_name, self.last_name)
 
+
+class Group(models.Model):
+    """ Group Model """
+
+    class Meta(object):
+        verbose_name = "Група"
+        verbose_name_plural = "Групи"
+
+    title = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name="Назва")
+
+    leader = models.OneToOneField('Student',
+        verbose_name="Староста",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL)
+
+    notes = models.TextField(
+        blank=True,
+        verbose_name="Додаткові нотатки",
+    )
+
+    def __str__(self):
+        if self.leader:
+            return "%s (%s %s)" % (self.title, self.leader.first_name, self.leader.last_name)
+        else:
+            return "%s" % (self.title,)
