2d0d66fed222dd2aeeb7a0c905f8ed1e590bf958
AlexaPyth <shakam77@gmail.com>
2015-10-28 11:35:32 +0200

    +імпорт моделі Group, Paginator +сортування і пагінація

--- students/views/groups.py
+++ students/views/groups.py
@@ -3,17 +3,40 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from students.models.groups import Group
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
-
-groups = (
-    {'id': 1, 'group_name': 'МтМ-21', 'starosta': 'Ячменев Олег' },
-    {'id': 2, 'group_name': 'МтМ-22',  'starosta': 'Петров Семен'  },
-    {'id': 3, 'group_name': 'МтМ-23', 'starosta': 'Іванов Олег' },
-)
+# groups = (
+#     {'id': 1, 'group_name': 'МтМ-21', 'starosta': 'Ячменев Олег' },
+#     {'id': 2, 'group_name': 'МтМ-22',  'starosta': 'Петров Семен'  },
+#     {'id': 3, 'group_name': 'МтМ-23', 'starosta': 'Іванов Олег' },
+# )
 
 
 # Views for Groups
 def groups_list(request):
+    groups = Group.objects.all()
+
+        # try to order Groups List
+    order_by = request.GET.get('order_by')
+    if order_by in ('id','title','leader'):
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse') == '1':
+            groups = groups.reverse()
+
+        # paginator Groups
+    paginator = Paginator(groups, 3)
+    page = request.GET.get('page')
+    try:
+        groups = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        groups = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver
+        # last page of results.
+        groups = paginator.page(paginator.num_pages)
+
     return render(request, "students/groups_list.html", {'groups':groups})
 
 def groups_add(request):
