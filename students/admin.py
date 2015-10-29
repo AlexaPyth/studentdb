fb6eb5be3472b5e933b0ffca0e3f786b36322797
AlexaPyth <shakam77@gmail.com>
2015-10-29 12:06:02 +0200

    добавив реєстрацію моделів Екзаменів та Викладачів

--- students/admin.py
+++ students/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from students.models.students import Student
 from students.models.groups import Group
+from students.models.lector import Lector
+from students.models.ekzamen import Ekzamen
+
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Group)
+admin.site.register(Lector)
+admin.site.register(Ekzamen)
